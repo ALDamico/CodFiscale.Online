3377aa4727079cbeee4eef29dc55ed9f3c976eb3
Andrea Luciano Damico <aldamico90@gmail.com>
2020-03-15 23:45:58 +0100

    Person form almost complete

--- src/components/PersonForm.vue
+++ src/components/PersonForm.vue
@@ -1,105 +1,130 @@
 <template>
-    <b-form class="container align-content-center">
-        <div class="card">
-            <div class="card-header">Informazioni personali</div>
-            <div class="card-body">
-                <!--<div class="form-group">-->
-                <div class="row p-3">
-                    <div class="col-3">
-                        <label for="personNameInput">Nome</label>
+    <div>
+        <b-alert variant="danger" :show="errorOccurred" dismissible fade>Si è verificato un errore durante il recupero
+            dei dati dal
+            server
+            remoto.
+        </b-alert>
+        <b-form class="container align-content-center">
+            <b-modal variant="danger" id="modalConfirmReset" @ok="resetForm">
+                Sei sicuro di voler reimpostare il modulo?<br>
+                Tutti i cambiamenti andranno persi!
+            </b-modal>
+            <div class="card">
+                <div class="card-header">Informazioni personali</div>
+                <div class="card-body">
+                    <!--<div class="form-group">-->
+                    <div class="row p-3">
+                        <div class="col-3">
+                            <label for="personNameInput">Nome</label>
+                        </div>
+                        <div class="col-9">
+                            <b-input class="mx-2 form-control" type="text" v-model="currentPerson.Name"
+                                     id="personNameInput"
+                                     placeholder="Nome"></b-input>
+                        </div>
                     </div>
-                    <div class="col-9">
-                        <b-input class="mx-2 form-control" type="text" v-model="currentPerson.Name" id="personNameInput"
-                                 placeholder="Nome"></b-input>
+                    <div class="row p-3">
+                        <div class="col-3">
+                            <label for="personSurnameInput">Cognome</label>
+                        </div>
+                        <div class="col-9">
+                            <b-input class="mx-2" type="text" v-model="currentPerson.Surname" placeholder="Cognome"
+                                     id="personSurnameInput"></b-input>
+                        </div>
                     </div>
-                </div>
-                <div class="row p-3">
-                    <div class="col-3">
-                        <label for="personSurnameInput">Cognome</label>
+                    <div class="row p-3">
+                        <div class="col-3">
+                            <label for="personBirthDateInput">Data di nascita</label>
+                        </div>
+                        <div class="col-9">
+                            <b-form-datepicker class="mx-2" v-model="currentPerson.BirthDate"
+                                               placeholder="Data di nascita"
+                                               id="personBirthDateInput"></b-form-datepicker>
+                        </div>
                     </div>
-                    <div class="col-9">
-                        <b-input class="mx-2" type="text" v-model="currentPerson.Surname" placeholder="Cognome"
-                                 id="personSurnameInput"></b-input>
+                    <div class="row p-3">
+                        <div class="col-3">
+                            <label for="personGenderInput">Sesso</label>
+                        </div>
+                        <div class="col-9">
+                            <b-form-select :options="allowedGenderValues" v-model="selectedGender" class="custom-select"
+                                           @change="changeGender" id="personGenderInput"></b-form-select>
+                        </div>
                     </div>
-                </div>
-                <div class="row p-3">
-                    <div class="col-3">
-                        <label for="personBirthDateInput">Data di nascita</label>
+                    <div class="row p-3">
+                        <div class="col-3">
+                            <label for="personBirthPlaceInput">Luogo di nascita</label>
+                            <font-awesome-icon v-show="loading" icon="spinner" transform="rotate"
+                                               class="fa-spin"></font-awesome-icon>
+                        </div>
+                        <div class="col-9">
+                            <Dropdown class="col-12"
+                                      :options="retrievedPlaces"
+                                      v-on:filter="searchPlace"
+                                      v-on:selected="changePlace"
+                                      placeholder="Luogo di nascita"
+                            ></Dropdown>
+                            <!--
+                            <b-dropdown variant="outline-primary" class="col-9" :text="placeHeader"
+                                        id="personBirthPlaceInput">
+                                <b-input class="m-0 p-0" placeholder="Cerca" type="text"
+                                         @change="searchPlace"></b-input>
+                                <b-dropdown-divider/>
+                                <div v-if="!this.loading && retrievedPlaces.length != 0">
+                                    <b-dropdown-item v-bind:key="place.Id" v-for="place in retrievedPlaces"
+                                                     @click="changePlace(place)">
+                                        {{place.name + " (" + place.provinceAbbreviation + ")"}}
+                                    </b-dropdown-item>
+                                </div>
+                                <div v-else-if="this.loading">
+                                    <b-dropdown-item>
+
+                                    </b-dropdown-item>
+                                </div>
+
+                            </b-dropdown>-->
+                        </div>
                     </div>
-                    <div class="col-9">
-                        <b-form-datepicker class="mx-2" v-model="currentPerson.BirthDate"
-                                           placeholder="Data di nascita" id="personBirthDateInput"></b-form-datepicker>
+                    <div class="row p-3 float-right">
+                        <b-btn variant="success" class="mx-5 button-rotate">
+                            <font-awesome-icon icon="check-circle"
+                                               class="button-animated-icon-check"></font-awesome-icon>
+                            Calcola
+                        </b-btn>
+                        <b-btn variant="danger" class="mx-5" @click="confirmReset">
+                            <font-awesome-icon class="button-animated-icon-undo" icon="undo"></font-awesome-icon>
+                            Reimposta
+                        </b-btn>
                     </div>
-                </div>
-                <div class="row p-3">
-                    <div class="col-3">
-                        <label for="personGenderInput">Sesso</label>
-                    </div>
-                    <div class="col-9">
-                        <b-form-select :options="allowedGenderValues" v-model="selectedGender" class="custom-select"
-                                       @change="changeGender" id="personGenderInput"></b-form-select>
-                    </div>
-                </div>
-                <div class="row p-3">
-                    <div class="col-3">
-                        <label for="personBirthPlaceInput">Luogo di nascita</label>
-                    </div>
-                    <div class="col-9">
-                        <b-dropdown variant="outline-primary" :text="placeHeader" id="personBirthPlaceInput">
-                            <b-input class="m-0 p-0" placeholder="Cerca" type="text"></b-input>
-                            <b-dropdown-divider/>
-                            <b-dropdown-item v-bind:key="place.Id" v-for="place in retrievedPlaces"
-                                             @click="changePlace(place)">
-                                {{place.name + " (" + place.provinceAbbreviation + ")"}}
-                            </b-dropdown-item>
-                        </b-dropdown>
-                    </div>
-                </div>
-                <div class="row p-3 float-right">
-                    <b-btn variant="success" class="mx-5 button-rotate">
-                        <font-awesome-icon icon="check-circle" class="button-animated-icon-check"></font-awesome-icon>
-                        Calcola
-                    </b-btn>
-                    <b-btn variant="danger" class="mx-5">
-                        <font-awesome-icon class="button-animated-icon-undo" icon="undo"></font-awesome-icon>
-                        Reimposta
-                    </b-btn>
-                </div>
 
 
+                </div>
             </div>
-        </div>
-        <!--</div>-->
-    </b-form>
+            <!--</div>-->
+        </b-form>
+    </div>
 </template>
 
 <script>
     import {Person} from "@/models/Person";
+    import ax from 'axios';
+    import Dropdown from 'vue-simple-search-dropdown';
 
     export default {
         name: "PersonForm",
+        components: {
+            Dropdown
+        },
         data: function () {
             return {
                 currentPerson: new Person(),
                 allowedGenderValues: ['Maschile', 'Femminile'],
                 selectedGender: null,
-                retrievedPlaces: [
-                    {
-                        name: 'Bronte',
-                        province: 'Catania',
-                        provinceAbbreviation: 'CT',
-                        code: 'B202',
-                        region: 'Sicilia'
-                    },
-                    {
-                        name: 'Randazzo',
-                        province: 'Catania',
-                        provinceAbbreviation: 'CT',
-                        code: 'H175',
-                        region: 'Sicilia'
-                    }
-                ],
-                selectedPlace: null
+                retrievedPlaces: [],
+                selectedPlace: null,
+                errorOccurred: false,
+                loading: false
             }
         },
         methods: {
@@ -109,9 +134,27 @@
             },
             changePlace: function (selectedPlace) {
                 this.currentPerson.BirthPlace = selectedPlace;
-                this.retrievedPlaces.push(selectedPlace);
                 this.selectedPlace = selectedPlace;
-                console.log(selectedPlace);
+            },
+            searchPlace: async function (partialName) {
+                this.loading = true;
+                const targetUrl = "https://localhost:5001/api/places?name=" + partialName;
+                await ax.get(targetUrl).then(result => {
+                    if (result.data === "") {
+                        this.retrievedPlaces = [];
+                    } else {
+                        this.retrievedPlaces = result.data;
+                    }
+                }).catch(() => {
+                    this.errorOccurred = true;
+                }).finally(() => this.loading = false)
+            },
+            confirmReset() {
+                this.$bvModal.show("modalConfirmReset");
+            },
+            resetForm() {
+
+                this.currentPerson = new Person();
             }
         },
         computed: {
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import {BootstrapVue} from "bootstrap-vue";
+
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckCircle, faUndo } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faUndo, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheckCircle, faUndo);
+library.add(faCheckCircle, faUndo, faSpinner);
 
 Vue.use(BootstrapVue);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
